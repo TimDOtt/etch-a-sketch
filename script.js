fe2314dcc29b0d5e3f4487a1414dc568ef9e8eb8
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-14 20:04:35 -0500

    Add mouseover effect

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const container = document.querySelector('.container');
+const row = document.querySelectorAll('.row');
+const col = document.querySelectorAll('.col');
 
 function createGrid(num) {
    for (i = 0; i < num; i++) {
     const row = document.createElement('div');
     container.appendChild(row);
     row.className = 'row';
-    
+    row.addEventListener('mouseover', color);
     for (j = 0; j < num; j++) {
         const col = document.createElement('div');
         col.className = 'col';
-        row.appendChild(col);   
+        row.appendChild(col);
+        col.addEventListener('mouseover', color);   
     }
    }
+}
+
+function color(e) {
+    let currentColor = 'green';
+    e.target.style.backgroundColor = currentColor;
 }
 
 createGrid(16);

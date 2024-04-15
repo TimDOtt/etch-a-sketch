2408f701d0e51337e5742fc9a4ef5fb3ad282759
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-14 19:16:26 -0500

    Center grid horizontally and vertically

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ function createGrid(num) {
     const row = document.createElement('div');
     container.appendChild(row);
     row.className = 'row';
-    row.textContent = i;
-
+    
     for (j = 0; j < num; j++) {
         const col = document.createElement('div');
         col.className = 'col';
-        row.appendChild(col);
-        col.textContent = j;
+        row.appendChild(col);   
     }
    }
 }

3a9cb40971f247fb5659529cafc3417eebfd04e3
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-15 00:36:34 -0500

    Fix child divs overflowing parent

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const userInput = document.querySelector('input');
+let containerWidth = getComputedStyle(container).width;
 
 userInput.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
@@ -11,16 +12,21 @@ userInput.addEventListener('keydown', function(event) {
     }
 })
 
+
+
 function createGrid(num) {
     container.innerHTML = '';
+    let squareSize = parseInt(containerWidth) / parseInt(num) - 2;
    for (i = 0; i < num; i++) {
     const row = document.createElement('div');
-    container.appendChild(row);
     row.className = 'row';
-    row.addEventListener('mouseover', color);
+    row.style.width = squareSize + 'px';
+    container.appendChild(row);
+    
     for (j = 0; j < num; j++) {
         const col = document.createElement('div');
         col.className = 'col';
+        col.style.height = squareSize + 'px';
         row.appendChild(col);
         col.addEventListener('mouseover', color);   
     }
@@ -31,5 +37,4 @@ function color(e) {
     let currentColor = 'purple';
     e.target.style.backgroundColor = currentColor;
 }
-
-// createGrid(16);
+createGrid(16);

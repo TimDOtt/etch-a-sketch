b7757256dac2ffb9b2c3f3e5627d1105cd5a27b6
Tim Ott <63533217+TimDOtt@users.noreply.github.com>
2024-04-17 03:55:40 -0500

    Add greyScale option

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container');
 const userInput = document.querySelector('input');
 const rainbow = document.querySelector('#rainbow');
-const clear = document.querySelector('#clearGrid')
+const clear = document.querySelector('#clearGrid');
+const greyScale = document.querySelector('#greyScale')
 let containerWidth = getComputedStyle(container).width;
 
 userInput.addEventListener('keydown', function(event) {
@@ -17,26 +18,38 @@ userInput.addEventListener('keydown', function(event) {
 function createGrid(num) {
     container.innerHTML = '';
     let squareSize = parseInt(containerWidth) / parseInt(num);
-   for (i = 1; i <= num; i++) {
+    for (i = 1; i <= num; i++) {
     const row = document.createElement('div');
     row.className = 'row';
     row.style.width = squareSize + 'px';
     container.appendChild(row);
     
-    for (j = 0; j < num; j++) {
-        const col = document.createElement('div');
-        col.className = 'col';
-        col.style.height = squareSize + 'px';
-        row.appendChild(col);
+        for (j = 0; j < num; j++) {
+            const col = document.createElement('div');
+            col.className = 'col';
+            col.style.height = squareSize + 'px';
+            row.appendChild(col);
+        }
     }
-   }
     solidColor();
     checkbox();
       
     rainbow.addEventListener('change', checkbox);
+    greyScale.addEventListener('click', grayScale);
     clear.addEventListener('click', function clearGrid() {
         createGrid(num);
+    
+
     })  
+}
+
+function grayScale(){
+    const square = document.querySelectorAll('.col');
+    square.forEach((square) => {
+        square.addEventListener('mouseover', function grey(){
+            square.style.opacity = +square.style.opacity + 0.1;
+        })
+    })
 }
 
 function checkbox() {
@@ -50,7 +63,7 @@ function solidColor() {
     const square = document.querySelectorAll('.col');
     square.forEach((square) => {
         square.addEventListener('mouseover', function color() {
-            square.style.backgroundColor = 'purple';
+            square.style.backgroundColor = 'black';
         });
     }); 
 }
